78a653424b8b076ac4d48f699ea555f4d9468c16
BilalCh0ta <smit.11.aandw@gmail.com>
2025-01-25 15:14:25 +0500

    changes in frontend

--- Client/src/Api/Api.jsx
+++ Client/src/Api/Api.jsx
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:3000/api/v1", // Remove extra slash
-    withCredentials: true, // To handle cookies
-  });
+  baseURL: "https://final-hackathon-smit.vercel.app/api/v1", // Corrected URL
+  withCredentials: true, // To handle cookies
+});
+
   
 
 export default API;
